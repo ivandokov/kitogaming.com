741b57aa936b32d1f149360fff28058ae3339743
Ivan Dokov <ivan@jetspark.io>
2020-03-30 14:07:37 +0300

    Add latest video to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="container px-3 flex flex-wrap justify-center my-12 py-12 lg:py-32">
-        <a href="https://www.facebook.com/todorov.kitodar/" target="_blank" class="px-8 py-4 bg-facebook rounded uppercase mb-4 mr-4">Facebook</a>
-        <a href="https://www.instagram.com/kitodar.fans/" target="_blank" class="px-8 py-4 bg-instagram rounded uppercase mb-4 mr-4">Instagram</a>
-        <a href="https://www.youtube.com/channel/UCqDkrKkZ2r2BHV3a0nIusIw" target="_blank" class="px-8 py-4 bg-youtube rounded uppercase mb-4">YouTube</a>
+    <div class="container px-3 mt-12">
+        <div class="flex flex-wrap justify-center mb-8">
+            <a href="https://www.facebook.com/todorov.kitodar/" target="_blank" class="px-8 py-4 bg-facebook rounded uppercase mb-4 mr-4">Facebook</a>
+            <a href="https://www.instagram.com/kitodar.fans/" target="_blank" class="px-8 py-4 bg-instagram rounded uppercase mb-4 mr-4">Instagram</a>
+            <a href="https://www.youtube.com/channel/UCqDkrKkZ2r2BHV3a0nIusIw" target="_blank" class="px-8 py-4 bg-youtube rounded uppercase mb-4">YouTube</a>
+        </div>
+        <div class="flex flex-wrap justify-center">
+            <div class="video-wrapper">
+                <iframe class="video" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ftodorov.kitodar%2Fvideos%2F766333157227594%2F&width=1520&height=928&show_text=false" width="500" height="280" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
+            </div>
+        </div>
     </div>
 </template>
