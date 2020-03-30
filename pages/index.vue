6a0a3fcdc0f7d96d5ff39ff77be9e4ee9c984cd9
Ivan Dokov <ivan@jetspark.io>
2020-03-30 14:08:40 +0300

    Add Discord link

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
         <div class="flex flex-wrap justify-center mb-8">
             <a href="https://www.facebook.com/todorov.kitodar/" target="_blank" class="px-8 py-4 bg-facebook rounded uppercase mb-4 mr-4">Facebook</a>
             <a href="https://www.instagram.com/kitodar.fans/" target="_blank" class="px-8 py-4 bg-instagram rounded uppercase mb-4 mr-4">Instagram</a>
-            <a href="https://www.youtube.com/channel/UCqDkrKkZ2r2BHV3a0nIusIw" target="_blank" class="px-8 py-4 bg-youtube rounded uppercase mb-4">YouTube</a>
+            <a href="https://www.youtube.com/channel/UCqDkrKkZ2r2BHV3a0nIusIw" target="_blank" class="px-8 py-4 bg-youtube rounded uppercase mb-4 mr-4">YouTube</a>
+            <nuxt-link to="/discord" target="_blank" class="px-8 py-4 bg-discord rounded uppercase mb-4">Discord</nuxt-link>
         </div>
         <div class="flex flex-wrap justify-center">
             <div class="video-wrapper">
