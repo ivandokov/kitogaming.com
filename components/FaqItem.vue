6f918bdf64681f829ccd153569c4715cd937c911
Ivan Dokov <ivan@jetspark.io>
2020-04-02 22:06:31 +0300

    Improve link zone

--- components/FaqItem.vue
+++ components/FaqItem.vue
@@ -1,7 +1,8 @@
 <template>
-    <div @click="opened = !opened" class="flex flex-wrap mb-4 cursor-pointer">
-        <div :class="{'text-red-700': open || opened}"
-             class="w-full text-2xl select-none hover:text-red-700"
+    <div class="flex flex-wrap mb-4">
+        <div @click="opened = !opened"
+             :class="{'text-red-700': open || opened}"
+             class="w-full text-2xl select-none hover:text-red-700 cursor-pointer"
         >{{ question }}</div>
         <div v-if="opened || open" class="pt-5"><slot/></div>
     </div>
