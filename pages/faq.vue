c1c70c27ed1210d02939e2b40f169e746d99f266
Ivan Dokov <ivan@jetspark.io>
2020-04-03 14:30:10 +0300

    Added one more Q & A

--- pages/faq.vue
+++ pages/faq.vue
@@ -4,8 +4,8 @@
             <h1 class="text-4xl mb-8">Често задавани въпроси</h1>
 
             <FaqItem id="discord-push-to-talk"
-                     class="mb-10"
                      :open="discordpushtotalk"
+                     class="mb-10"
                      question="Как да си настроя Push-to-Talk бутона в Discord">
                 <p class="mb-8">
                     В някои канали в Discord се изисква да имате настроен бутон с който да активирате микрофона си (Push-to-Talk). Това можете да направите от настройките на Discord. Ако играта, която играете също има възможност за аудио разговор и се изисква бутон за активиране на микрофона настройте бутона да е различен от този в Discord, защото ще се чувате два пъти, което не е приятно за зрителите.
@@ -15,8 +15,8 @@
             </FaqItem>
 
             <FaqItem id="dota2id"
-                     class="mb-10"
                      :open="dota2id"
+                     class="mb-10"
                      question="Как да намеря моето ID в Dota 2">
                 <p class="mb-8">
                     Можете да го видите като влезете в играта, натиснете портрета си и видите ID-то си под името.
@@ -25,8 +25,8 @@
             </FaqItem>
 
             <FaqItem id="dota2party-invite"
-                     class="mb-10"
                      :open="dota2partyinvite"
+                     class="mb-10"
                      question="Как да махна блокирането да бъда поканен в партия от непознати играчи?">
                 <p class="mb-8">
                     За да можете да бъдете поканени от Кито, трябва да позволите покани от непознати хора. Това става когато натиснете зъбчатото колело в горния ляв ъгъл на играта, натиснете Options, Advanced Options и изключите "Block party invites from non-friends"
@@ -37,8 +37,8 @@
             </FaqItem>
 
             <FaqItem id="csgo-console"
-                     class="mb-10"
                      :open="csgoconsole"
+                     class="mb-10"
                      question="Как да активирам конзолата в Counter-Strike: Global Offensive?">
                 <p class="mb-8">
                     Кликнете зъбчатото колело в ляво и в раздел "Game" нагласете "Enable Developer Console (~)" на "Yes"
@@ -47,13 +47,22 @@
             </FaqItem>
 
             <FaqItem id="csgo-connect"
-                     class="mb-10"
                      :open="csgoconnect"
+                     class="mb-10"
                      question="Как да се свържа с Counter-Strike: Global Offensive сървър?">
                 <p class="mb-8">
                     Включете конзолата с бутона tilde (~) в ляво от 1 / под ESC и напишете команда "connect" (без кавички) последвана от IP адреса
                 </p>
                 <img src="/cs-connect.jpg" alt="cs connect" class="rounded shadow-2xl">
+            </FaqItem>
+
+            <FaqItem id="suggest-game"
+                     :open="suggestgame"
+                     class="mb-10"
+                     question="Как да предложа игра на Кито">
+                <p>
+                    Можете да предложите игра, която Кито да играе в стрийма като напишете предложението си в Discord в канала #предложи-игра
+                </p>
             </FaqItem>
         </div>
     </div>
@@ -73,6 +82,7 @@
                 dota2partyinvite: false,
                 csgoconsole: false,
                 csgoconnect: false,
+                suggestgame: false,
             }
         },
         mounted() {
