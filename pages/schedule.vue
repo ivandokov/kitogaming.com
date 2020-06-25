dc4f79fe9247c410c74e225eadd530577e5fa5af
Ivan Dokov <ivan@jetspark.io>
2020-06-25 10:14:00 +0300

    Link the schedule from external site

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-4 lg:px-12 pt-4 lg:pt-12">
-        <img class="rounded shadow-2xl" src="/week-21.png" alt="schedule">
+        <img class="rounded shadow-2xl" src="http://schedule.kitogaming.com/schedule.png" alt="schedule">
     </div>
 </template>
 
